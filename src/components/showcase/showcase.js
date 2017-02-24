fc8f28c544a9c18311fd23526f736b16de89d10e
pragyandas <pragyandas@msn.com>
2017-02-25 00:54:08 +0530

    modified styling

--- src/components/showcase/showcase.js
+++ src/components/showcase/showcase.js
@@ -15,7 +15,7 @@ class Showcase extends Component{
           </div>
           <div className={style.contentTwo}>
             <div className={style.description}>
-              <h4>Self-Cooking</h4>
+              <h4>Self Cooking</h4>
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
             <div className={style.imageTwo}>
